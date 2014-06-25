99daa99d0fa618b6c12c5ceb75275dd92bdc52e7
nickkaranatsios@gmail.com <nickkaranatsios@gmail.com>
2014-06-25 21:39:32 +0900

    openstack test program

---
+++ os-test.rb
@@ -0,0 +1,12 @@
+require "rubygems"
+require "openstack"
+require "pp"
+os = OpenStack::Connection.create({:username => "admin", :api_key=>"stack", :auth_method=>"password", :auth_url => "http://192.168.1.5:5000/v2.0/", :authtenant_name =>"admin", :service_type=>"compute"})
+servers = os.servers
+pp servers
+pp os.get_floating_ips
+vs = OpenStack::Connection.create({:username => "admin", :api_key=>"stack", :auth_method=>"password", :auth_url => "http://192.168.1.5:5000/v2.0/", :authtenant_name =>"admin", :service_type=>"volume"})
+#volume = vs.create_volume({:display_name=>"nick volume", :size=>1, :display_description=>"test nick volume"})
+pp vs.volumes
+#pp vs.list_volumes
+vs.delete_volume("6a02276a-0d8a-4f57-8a84-73f22b3c5731")
