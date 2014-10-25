b11667962e06d5c69a9f60ccd920f9a85c979d18
nickkaranatsios@gmail.com <nickkaranatsios@gmail.com>
2014-10-25 16:30:28 +0900

    another way to create a power set

--- power-set.rb
+++ power-set.rb
@@ -1,8 +1,20 @@
 require "pp"
 
+def next_seq
+  (1..1.0/0).lazy.each
+end
+
 # computes the powerset of an array
 a = Array.new( 4 ) { | e | e + 1 }
-pp a
+e = next_seq
+
+ps = []
+begin
+  num = e.next
+  a.combination(num).inject(ps,:<<)
+end while num < a.length
+pp ps
+
 ps = []
 ps << []
 ( 1..a.length ).each do | e |
