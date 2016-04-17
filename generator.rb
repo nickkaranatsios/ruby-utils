e89f5b395ccc014cc9d0635d0551dda7badbd04b
nickkaranatsios@gmail.com <nickkaranatsios@gmail.com>
2016-04-17 15:32:52 +0900

    added some output

--- generator.rb
+++ generator.rb
@@ -1,15 +1,32 @@
 require 'digest/bubblebabble'
 
 class Generator
-  def initialize
+  def initialize no_times
     @random_arr = ("a".."z").to_a + ("A".."Z").to_a + ("0".."9").to_a
+    @no_times = no_times
   end
+
+
+  def output_mutation_to
+    shuffle_string = shuffle
+    shuffle_array = shuffle_string.split("-")
+    f1, f2, f3 = shuffle_array.each_slice(shuffle_array.size / 3).to_a
+    puts f1.join("-")
+    puts f2.join("-")
+    puts f3.join("-")
+    puts shuffle_string
+    @no_times.times do | each |
+      t = Time.now
+      t.strftime("%m/%d/%Y %H:%I:%S")
+      
+    end 
+  end
+
+  private
 
   def shuffle
     Digest::SHA256.bubblebabble mutate_population
   end
-
-  private
 
   def mutate_population
     @random_arr.shuffle.take(10).join
@@ -18,5 +35,13 @@ end
 
 #t.strftime("%m/%d/%Y %H:%I:%S")
 # a = "2" * 10
-generator = Generator.new
-puts generator.shuffle
+# "X" * 3 - sa001
+# cnt = cnt + 1
+# "NG"
+# "3"
+# "mail"
+# 4 file names separated with spaces + 1 space at the end
+# size of file in size for a maximum of 2 file names
+no_times = ARGV[0] || 1000
+generator = Generator.new no_times
+generator.output_mutation_to
